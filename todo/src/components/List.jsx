300e4de8e24e91a6dcea42b675f08bd6a486ec5a
gudals <gudals@gmail.com>
2025-04-18 00:37:29 +0900

    go

---
+++ todo/src/components/List.jsx
@@ -0,0 +1,22 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+const List = () => {
+  return (
+    <div className="List">
+      <h4>Todo List🌿</h4>
+      <input
+        type="text"
+        className="search"
+        placeholder="검색어를 입력하세요."
+      />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default List;
